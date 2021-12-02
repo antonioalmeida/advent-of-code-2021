722c2eabd98c8b584b251f65934d5faec2c640e6
antonioalmeida <theantonioalmeida@gmail.com>
2021-12-03 00:53:03 +0100

    day 2 part 2

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -1,19 +1,19 @@
 import { readInput } from '../utils'
 
-const commands = readInput('day2.in').map((entry) => {
+const commands: { direction: string, amount: number }[] = readInput('day2.in').map((entry) => {
     const [direction, amount] = entry.split(' ')
-    return { direction, amount } 
+    return { direction, amount: Number(amount) } 
 })
  
-const ops = {
-    'up':      (n) => (x,y) => [Number(x),Number(y)-Number(n)],
-    'forward': (n) => (x,y) => [Number(x)+Number(n),Number(y)],
-    'down':    (n) => (x,y) => [Number(x),Number(y)+Number(n)],
-}
 
 export const part1 = () => {
-    let x = 0
-    let y = 0
+    const ops = {
+        'up':      (n) => (x,y) => [x,y-n],
+        'forward': (n) => (x,y) => [x+n,y],
+        'down':    (n) => (x,y) => [x,y+n],
+    }
+
+    let x = 0; let y = 0
 
     for (const {direction, amount} of commands) {
         const op = ops[direction](amount);
@@ -23,7 +23,19 @@ export const part1 = () => {
     return x*y
 }
 
-
 export const part2 = () => {
-    
+    const ops = {
+        'up':      (n) => (x,y,a) => [x,y,a-n],
+        'forward': (n) => (x,y,a) => [x+n,y+(a*n),a],
+        'down':    (n) => (x,y,a) => [x,y,a+n],
+    }
+
+    let x = 0; let y = 0; let a = 0;
+
+    for (const {direction, amount} of commands) {
+        const op = ops[direction](amount);
+        [x,y,a] = op(x,y,a)
+    }
+
+    return x*y
 }

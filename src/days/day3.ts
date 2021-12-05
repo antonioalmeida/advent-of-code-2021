5b097dea1f4e6c0a86c03085164907a530e3ddf6
antonioalmeida <theantonioalmeida@gmail.com>
2021-12-05 02:42:37 +0100

    day 3 part 2

--- src/days/day3.ts
+++ src/days/day3.ts
@@ -3,8 +3,8 @@ import { readInput } from '../utils'
 const report = readInput('day3.in')
  
 export const part1 = () => {
-    let gamma = '';
-    let i = -1; let j = -1;
+    let gamma = ''
+    let i = -1, j = -1 
 
     while (++i < report[0].length) {
         let colSum = 0
@@ -21,6 +21,25 @@ export const part1 = () => {
     return decimalGamma * decimalEpsilon
 }
 
-export const part2 = () => {
+const getRating = (report, bitCriteria: (a: number, b: number) => string ) => {
+    let i = -1, j = -1 
+    let relevant = report
+    while (relevant.length > 1 && ++i < relevant[0].length) {
+        let colSum = 0;
+        while (++j < relevant.length)
+            colSum += Number(relevant[j][i]);
+        const target = bitCriteria(colSum, relevant.length)
 
+        relevant = relevant.filter((row) => row[i] == target)
+        j = -1;
+    }
+
+    return parseInt(relevant, 2)
+}
+
+export const part2 = () => {
+    const oxygen = getRating(report, ((sum, size) => sum >= size / 2 ? "1" : "0"))
+    const co2 = getRating(report, ((sum, size) => sum >= size / 2 ? "0" : "1"))
+
+    return oxygen * co2
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,6 @@ import fs from 'fs'
 
 const BASE_DIR = 'inputs'
 
-export const readInput = (filename, separator: string = '\n') => {
+export const readInput = (filename: string, separator: string = '\n') => {
     return fs.readFileSync(`${BASE_DIR}/${filename}`).toString().split(separator)
 }

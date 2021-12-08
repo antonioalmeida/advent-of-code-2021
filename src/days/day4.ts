08c9eb1f1a1e4a0850adbb1c820a721961c83b74
antonioalmeida <theantonioalmeida@gmail.com>
2021-12-08 01:59:40 +0100

    day 4 part 2

--- src/days/day4.ts
+++ src/days/day4.ts
@@ -21,5 +21,22 @@ export const part1 = () => {
 }
 
 export const part2 = () => {
+    let lastScore: number
 
+    for (const d of draws) {
+        for (let i = 0; i < boards.length; i++) {
+            const b = boards[i].map((row) => row.map(value => value == d ? -1 : value))
+            const sum = b.map(r => r.filter(c => c > -1).reduce((a,b) => a+b, 0)).reduce((a,b) => a+b)
+            const hBingo = b.map(r => r.reduce((a,b) => a+b, 0)).some((u) => u == -5)
+            const vBingo = transpose(b).map(r => r.reduce((a,b) => a+b, 0)).some((u) => u == -5)
+
+            if (vBingo || hBingo) {
+                lastScore = sum * d
+                boards.splice(i, 1)
+                i--
+            } else
+                boards[i] = b
+        }
+    }
+    return lastScore
 }
